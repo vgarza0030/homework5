9dc75e5396f4198184f8d53d985f3a038667fcc4
Victor Garza <vgarza_0030@stu.southtexascollege.edu>
2025-02-18 13:44:57 -0600

    Card Componetns Working

---
+++ src/components/MainCards.vue
@@ -0,0 +1,13 @@
+<script setup>
+import MainCardSingle from './MainCardSingle';
+
+
+</script>
+
+
+<template>
+    <MainCardSingle v-for="card in 20" :key="card"></MainCardSingle>
+
+
+
+</template>
